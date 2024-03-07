51702fd532c8f26e5891684bde77076e5930c467
Violet Vivirand <purpleslovemail@gmail.com>
2024-03-07 14:35:52 +0800

    Refine startup script

--- serve.sh
+++ serve.sh
@@ -1,3 +1,11 @@
-/bin/bash -c "nohup ollama serve &> ollama_log.txt &" && \
-  sleep 1 && \
-  ollama run gemma:2b-instruct-q4_0 $1
+echo "[INFO] Launching Ollama"
+bash -c "nohup ollama serve > /ollama_log.txt 2>&1 &"
+sleep 1
+
+echo "[INFO] Make inference"
+ollama run gemma:2b-instruct-q4_0 $1
+
+echo "[INFO] Kill Ollama Process"
+kill -9 $(pgrep -f "ollama serve")
+
+echo "[INFO] Done."
